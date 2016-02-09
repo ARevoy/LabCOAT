16a42218d9283900746f64d3ac16f55cd1e51ea5
ARevoy <andrew.revoy@gmail.com>
2016-02-09 15:35:33 -0500

    Login page initial
    Added login page styling

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php include 'header.php';?>
+
+<div class="container" >
+<div class="col-md-3"></div>
+	<div class="col-md-6 panel panel-default">
+		<div class="jumbotron text-center set-white">
+		<h3>Login:</h3>
+		<div class="input-group">
+				<span class="input-group-addon">First Name</span>
+				<input type="text" class="form-control">
+			</div>
+		<div class="input-group">
+				<span class="input-group-addon">Last Name</span>
+				<input type="text" class="form-control">
+			</div>
+		<div class="input-group">
+				<span class="input-group-addon">Faculty/Student ID</span>
+				<input type="text" class="form-control">
+			</div>
+		<button class="btn btn-default">Login</button>
+		</div><!-- /jumbotron -->	
+	</div>
+<div class="col-md-4"></div>
+</div>
+<?php include 'footer.php';?>
