82faa882bff2ec2879a9c32901334159fef06500
ARevoy <andrew.revoy@gmail.com>
2016-02-18 19:51:51 -0500

    CSV Parser
    PapaParser script added to parse uploaded csv files.

--- import.php
+++ import.php
@@ -1,4 +1,15 @@
 <?php include 'header.php'; ?>
+<script src = "/javascript/script.js"></script>
+<div class="container panel panel-default">
+	<div class="jumbotron text-center">
+		<h2>Import tool:</h2>
+		<label class="label label-default" for="files">Select a file to upload:</label>
+		<input class="container" type="file" name="files" id="files">
+
+		<div class="btn btn-default" id="submit">Upload!</div>
+		
+	</div>
+</div>
 
 
 
